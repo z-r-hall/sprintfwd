1ff0b675aeb1c350628ecc02136af93523321e7e
Zach <zachh85@gmail.com>
2023-04-19 00:48:23 -0400

    update persisting state

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,22 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import todo from './mock-db/todo';
 
 function Todo() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState(todo.items);
   const [todoItem, setTodoItem] = useState('');
   const [showUpdateInput, setShowUpdateInput] = useState(false);
   const [updateInput, setUpdateInput] = useState('');
   const [todoIndex, setTodoIndex] = useState();
 
+  useEffect(()=> {
+    setTodoList(todo.items);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[todo.items])
+
   function addItem(e) {
-    setTodoList([...todoList, todoItem]);
     setShowUpdateInput(false);
+    todo.items = [...todo.items, todoItem];
     document.querySelector('input').value = '';
   }
 
   function deleteItem(index){
-    const newList = todoList.filter((el, i) => i !== index);
     setShowUpdateInput(false);
-    setTodoList(newList);
+    todo.items = todo.items.filter((el, i) => i !== index);
   }
 
   function updateItem(index){
@@ -26,26 +31,25 @@ function Todo() {
   }
   function submitUpdate() {
     setShowUpdateInput(false);
-    setTodoList(todoList.map((el, i)=> {
+    todo.items = todo.items.map((el, i)=> {
       if(i === todoIndex) {
-        el = updateInput;
+        el = updateInput
         return el;
       } else {
         return el;
       }
-    }))
+    });
   }
   return (
     <div className="Todo" >
       <input onChange={(e)=>setTodoItem(e.target.value)} /><button onClick={addItem} >Add Item</button><br/>
       
       {showUpdateInput && <div><input value={updateInput} onChange={(e)=> setUpdateInput(e.target.value)} /><button onClick={submitUpdate} >Update Item</button><br/></div>}
-
+      <h3>To-do List</h3>
       <ul>
         {todoList.map((el, i) => {
           return (
-          <li key={i}>{el} <button onClick={()=>deleteItem(i)} >Delete</button> <button onClick={()=>updateItem(i)} >Update</button></li>
-          )
+          <div key={i} className="todo-buttons"><li>{el} </li><div><button onClick={()=>deleteItem(i)} >Delete</button><button onClick={()=>updateItem(i)} >Update</button></div></div>)
         })}
       </ul>
     </div>
