d6eff0a0c44f342434780e662515eefc46f568fe
Zach <zachh85@gmail.com>
2023-04-19 08:20:45 -0400

    welcome margin

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
       setTeam={setTeam} />}
       {showTodo && <Todo />}
       {showTeamDetails && <TeamDetails team={team} />}
-      {showWelcome && <div><h2>Welcome!</h2><h4>Choose a tab above to get started.</h4><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>}
+      {showWelcome && <div><h2>Welcome!</h2><div>Choose a tab above to get started.</div><br/><p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p></div>}
     </div>
   );
 }
